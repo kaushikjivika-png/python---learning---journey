409e96b3e7abac27c5a0683c333f13174880c0f3
Jivika Kaushik <kaushikjivika@gmail.com>
2026-01-06 00:16:00 +0530

    Add files via upload

---
+++ day01_basics.py
@@ -0,0 +1,39 @@
+# PYTHON
+
+# Day 1: Python basics
+# Topics covered: variables, data types, input/output, basic operations
+
+# 1. variables:
+
+# name = "jivika kaushik"
+# age = 20
+# height = 5.6
+# is_student = True
+
+# print("name:",name)
+# print("age:",age)
+# print("height:",height)
+# print("is_student:",is_student)
+
+# # 2. checking data types:
+
+# print(type(name))                   # str
+# print(type(age))                    # int
+# print(type(height))                 # float
+# print(type(is_student))             # bool
+
+# 3. taking user input:
+
+# city = input("enter your city:")
+# print("hello",city)
+
+# 4. basic operations:
+
+a = 10
+b = 3
+# print("addition:",a+b)               # 13
+# print("subtraction:",a-b)            # 7
+# print("multiplication:",a*b)         # 30
+# print("division:",a/b)               # 3.3333
+# print("reminder:",a%b)                # 1
+# print("expomentiation:",a**b)       # 1000
