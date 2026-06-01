a32ba68cc1389455b836ffbdd19fdd27c6c35922
Jivika Kaushik <kaushikjivika@gmail.com>
2026-06-01 13:31:22 +0530

    Added Frequency Counter Program.

--- day09_basic_Questions.py
+++ day09_basic_Questions.py
@@ -765,3 +765,18 @@ for ch in s:
     if ch in c:
         count += 1
 print(count)
+
+# question 56: Frequency Counter.
+
+sentence = input("Enter your sentence : ")
+words = sentence.split()
+
+frequency = {}
+
+for word in words:
+    if word in frequency:
+        frequency[word] += 1
+    else:
+        frequency[word] = 1
+for word, count in frequency.items():
+    print(word, ":", count)
